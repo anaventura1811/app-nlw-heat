eeaea70ffae36a7856e8948a41ccdcde36a1cbd2
Ana Ventura <anaventura1811@gmail.com>
2021-10-28 10:28:21 -0300

    cria rota de callback para signin

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,13 @@ const app = express();
 const PORT = 4000;
 
 app.get('/github', (_req, res) => {
-  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
-})
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
+
+app.get('/signin/callback', (req, res) => {
+  const { code } = req.query;
+
+  return res.json(code);
+});
 
 app.listen(PORT, () => console.log(`ouvindo na porta ${PORT}`));
