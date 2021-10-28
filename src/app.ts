c72d976b7083ba4d4670a809a3599090a8342bdc
Ana Ventura <anaventura1811@gmail.com>
2021-10-28 10:24:38 -0300

    adiciona configuração de variáveis de ambiente, cria rota get de autenticação com github oauth para teste

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
-import express from 'express';
+import 'dotenv/config';
+import express, { response } from 'express';
 
 const app = express();
 const PORT = 4000;
+
+app.get('/github', (_req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
 
 app.listen(PORT, () => console.log(`ouvindo na porta ${PORT}`));
