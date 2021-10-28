e9e405e3b4fd01ebc404f7840a8bc551e4ab7f53
Ana Ventura <anaventura1811@gmail.com>
2021-10-28 11:24:28 -0300

    adiciona axios às dependências do projeto, configura chamada de autenticação

--- src/services/authenticateUserService.ts
+++ src/services/authenticateUserService.ts
@@ -1,3 +1,27 @@
+import "dotenv/config";
+import axios from "axios";
+
+class AuthenticaUserService {
+  async execute(code: string) {
+    const url = 'https://github.com/login/oauth/access_token';
+
+    const response = await axios.post(url, null, {
+      params: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+      },
+      headers: {
+        "Accept": "application/json"
+      }
+    });
+
+    return response.data;
+  }
+}
+
+export { AuthenticaUserService }
+
 /* Receber o código via string, 
 recuperar o access token no github para
 ter acesso às informações do usuário
@@ -6,11 +30,3 @@ Se existir, gera um token
 Se não existir, cria no DB e gera um token
 Retorna o token com as infos do user logado
 */
-
-class AuthenticaUserService {
-  async execute(code: string) {
-
-  }
-}
-
-export { AuthenticaUserService }
