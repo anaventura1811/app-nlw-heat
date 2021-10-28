9c8f463aa3feba1305ed417ef8be74467ca96118
Ana Ventura <anaventura1811@gmail.com>
2021-10-28 18:33:07 -0300

    feat: inicia configurações de socket

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,31 @@
 import 'dotenv/config';
 import express from 'express';
 import http from 'http';
+import cors from 'cors';
 import { Server } from 'socket.io';
 
 import { router } from './routes/routes';
 
 const app = express();
+app.use(cors());
 
 const serverHttp = http.createServer(app);
 
-const io = new Server(serverHttp);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
 
 const PORT = process.env.PORT || 4000;
 
 
 app.use(express.json());
+
 app.use(router);
 
 app.get('/github', (_req, res) => {
