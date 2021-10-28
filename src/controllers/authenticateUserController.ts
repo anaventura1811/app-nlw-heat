86e444a9e5096182e3b18c900cb59491cfe743ac
Ana Ventura <anaventura1811@gmail.com>
2021-10-28 11:18:52 -0300

    rascunho das regras de negócio de autenticação

--- src/controllers/authenticateUserController.ts
+++ src/controllers/authenticateUserController.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
+import { AuthenticaUserService } from '../services/authenticateUserService';
 
 class AuthenticaUserController {
   async handle(req: Request, res: Response) {
     
+
+    const service = new AuthenticaUserService();
+    // service.execute();
   }
 }
 
--- src/services/authenticateUserService.ts
+++ src/services/authenticateUserService.ts
@@ -1,6 +1,15 @@
+/* Receber o código via string, 
+recuperar o access token no github para
+ter acesso às informações do usuário
+Verificar se usuário existe no DB
+Se existir, gera um token
+Se não existir, cria no DB e gera um token
+Retorna o token com as infos do user logado
+*/
+
 class AuthenticaUserService {
   async execute(code: string) {
-    
+
   }
 }
 
